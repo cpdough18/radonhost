7bfeef935136fa49b0c9b3d9175b0c29fc7329a6
Foo <34515228+Fooooooooooooooo@users.noreply.github.com>
2018-09-05 09:58:26 -0700

    fuck

--- Core/Configuration.cs
+++ Core/Configuration.cs
@@ -8,33 +8,32 @@ namespace Radon.Core
 {
     public class Configuration
     {
-        [JsonProperty("BotDiscordInviteLink")] public readonly string BotDiscordInviteLink = "https://discordapp.com/oauth2/authorize?client_id=481695914801496065&permissions=8&scope=bot";
-
+        [JsonProperty("BotDiscordInviteLink")] public readonly string BotDiscordInviteLink = "";
         [JsonProperty("BotPrefixes")] public readonly string[] BotPrefixes = { "$" };
 
-        [JsonProperty("BotToken")] public readonly string BotToken = "NDgxNjk1OTE0ODAxNDk2MDY1.DmAIRg.SLjjjG8aNokJaldpsPIf_tAdNKo";
+        [JsonProperty("BotToken")] public readonly string BotToken = "";
 
         [JsonProperty("DatabaseConnectionString")] public readonly string DatabaseConnectionString = "http://127.0.0.1:8080";
 
-        [JsonProperty("DefaultJoinMessage")] public readonly string DefaultJoinMessage = "bop";
+        [JsonProperty("DefaultJoinMessage")] public readonly string DefaultJoinMessage = "";
 
-        [JsonProperty("DefaultLeaveMessage")] public readonly string DefaultLeaveMessage = "pob";
+        [JsonProperty("DefaultLeaveMessage")] public readonly string DefaultLeaveMessage = "";
 
-        [JsonProperty("DefaultLevelUpMessage")] public readonly string DefaultLevelUpMessage = "Eat ass";
+        [JsonProperty("DefaultLevelUpMessage")] public readonly string DefaultLevelUpMessage = "";
 
-        [JsonProperty("GiphyApiKey")] public readonly string GiphyApiKey = "YsZ9PGPpZcYu7NVpYpXUCKvgHNbLUFwf";
+        [JsonProperty("GiphyApiKey")] public readonly string GiphyApiKey = "";
 
         [JsonProperty("GitHubLink")] public readonly string GitHubLink = "";
 
-        [JsonProperty("KsoftApiKey")] public readonly string KsoftApiKey = "533847961cdb71e867a0445f768a324971477598";
+        [JsonProperty("KsoftApiKey")] public readonly string KsoftApiKey = "";
 
         [JsonProperty("LiscordApiKey")] public readonly string LiscordApiKey = "";
 
         [JsonProperty("LolApiKey")] public readonly string LolApiKey = "";
 
-        [JsonProperty("e621UserAgent")] public readonly string E621UserAgent = "fooooooooooooooo";
+        [JsonProperty("e621UserAgent")] public readonly string E621UserAgent = "";
 
-        [JsonProperty("OwnerIds")] public readonly ulong[] OwnerIds = { 131283250042634241 };
+        [JsonProperty("OwnerIds")] public readonly ulong[] OwnerIds = {  };
 
         [JsonProperty("ShardCount")] public readonly int ShardCount = 1;
     }
